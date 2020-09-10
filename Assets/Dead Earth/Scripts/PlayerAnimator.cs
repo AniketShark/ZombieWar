2105aa655a7b7f2e6b3d44a6b277637b4acc1473
AniketShark <aniket.kayande@gmail.com>
2020-09-10 18:44:05 +0530

    [Learned] Animation Blend Tree. [Added] UniRx Framework.

--- Assets/Dead Earth/Scripts/PlayerAnimator.cs
+++ Assets/Dead Earth/Scripts/PlayerAnimator.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Animator _animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int _horizontalHash;
+	public int _verticalHash;
+	public int _attackHash;
+
+	void Start()
+	{
+		_horizontalHash = Animator.StringToHash("horizontal");
+		_verticalHash = Animator.StringToHash("vertical");
+		_attackHash = Animator.StringToHash("attack");
+	}
+	void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal") * 2.32f;
+		float vertical = Input.GetAxis("Vertical") * 5.67f;
+		bool attack = Input.GetMouseButtonDown(0);
+		//Debug.LogError(string.Format("Horizontal {0} Vertical {1}  ", horizontal, vertical));
+		_animator.SetFloat(_horizontalHash, horizontal,0.1f,Time.deltaTime);
+		_animator.SetFloat(_verticalHash, vertical,1.0f, Time.deltaTime);
+		if(attack)
+			_animator.SetTrigger(_attackHash);
+	}
 }
